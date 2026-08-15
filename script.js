6338b99064484c18b52e11ec5a7fa9c3571d8a73
BobBrian <rellrocker235@gmail.com>
2026-08-15 18:30:51 -0230

    Adding a hover effect to change the color of squares made by the div

--- script.js
+++ script.js
@@ -7,10 +7,19 @@ function createGrid(size) {
   const totalSquares = size * size;
   
   for (let i = 0; i < totalSquares; i++) {
+
     //document.createElement is a DOM operation that allows us to create HTML elements
     const square = document.createElement("div");
+
     //classList.add is a DOM operation that allows us to add CSS to a HTML element
     square.classList.add("grid-square");
+
+    //This is an event lister that give the mouse a hover effect changing the color of the squares
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "black";
+    });
+
+
     //.appendChild is a DOM operation that allows us adds a specified node as the last child of a parent node. In this case the div with 
     // the container id is the parent node and the div created by our square vairable is the last child
     container.appendChild(square);
